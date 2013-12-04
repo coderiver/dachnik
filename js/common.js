67822a755f185f7c74047ffefb5ad5ab316ec97e
Georgiy <goshazvir@gmail.com>
2013-12-04 11:34:08 +0200

    footer and header

--- js/common.js
+++ js/common.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+	// STICKY FOOTER
+	$(function() {
+		var footerHeight = $(".footer").height();
+		$(".out").css("margin-bottom", -footerHeight);
+		$(".push").css("height", footerHeight);
+	});
 
+	$(window).resize(function() {
+		var footerHeight = $(".footer").height();
+		$(".out").css("margin-bottom", -footerHeight);
+		$(".push").css("height", footerHeight);
+	});
 
 });
