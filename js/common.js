a6b40e3a82412bbef55784ea3217863ab67939f8
Georgiy <goshazvir@gmail.com>
2013-12-09 18:30:26 +0200

    load

--- js/common.js
+++ js/common.js
@@ -13,4 +13,40 @@ $(document).ready(function() {
 	// 	$(".push").css("height", footerHeight);
 	// });
 
+	//select
+	function select() {
+		var el = $('.js-select');
+		var el_title = el.children("span");
+		var item = el.find('li');
+		var input = el.find(".js-select-input");
+		var el_text = el.find(".js-select-text");
+		var checkbox = el.find(".checkbox");
+		var list = el.find('.js-select-drop');
+		el_title.click(function(event){
+			if ($(this).parent().hasClass('is-open')) {
+				$(this).parent().removeClass('is-open');
+			}
+			else {
+				el.removeClass('is-open');
+				$(this).parent().addClass('is-open');
+			};
+			event.stopPropagation();
+		});
+		checkbox.click(function(event){
+			event.stopPropagation();
+		});
+		item.bind("click",function(){
+			$(this).toggleClass("is-selected");
+			var text = $(this).text();
+			var id = $(this).attr("data-id");
+			$(this).parents(".js-select").find(".js-select-text").text(text);
+			$(this).parents(".js-select").find(".js-select-input").val(id);
+	 });
+	};
+	select();
+
+	$(document).click(function() {
+		$('.js-select').removeClass('is-open');
+	});
+
 });
