da5bd792fe24f558435c257f7842926e925050c0
akella <akella.a@gmail.com>
2014-03-09 17:46:56 +0200

    sticky panel

--- js/common.js
+++ js/common.js
@@ -95,4 +95,33 @@ $(document).ready(function() {
 	  });
 	}
 	tab();
+
+
+
+	// var stickyPanelSettings = {
+ //        // Use this to set the top margin of the detached panel.
+ //        topPadding: 10,
+
+ //        // This class is applied when the panel detaches.
+ //        afterDetachCSSClass: "",
+
+ //        // When set to true the space where the panel was is kept open.
+ //        savePanelSpace: true,
+
+ //        // Event fires when panel is detached
+ //        // function(detachedPanel, panelSpacer){....}
+ //        onDetached: null,
+
+ //        // Event fires when panel is reattached
+ //        // function(detachedPanel){....}
+ //        onReAttached: null,
+
+ //        // Set this using any valid jquery selector to 
+ //        // set the parent of the sticky panel.
+ //        // If set to null then the window object will be used.
+ //        parentSelector: null
+ //    };
+ //    $(".l-sidebar").stickyPanel(stickyPanelSettings);
+
+ $(".l-sidebar_r").sticky({topSpacing:10});
 });
